94f0d84429f85ead940495cb636bd50fa766bc54
David DiBenedetto <david@njweb.solutions>
2022-12-14 19:53:26 -0500

    Discord commands
    Write discord bot commands

---
+++ commands/echo.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption((option) => option.setName('input')
+      .setDescription('The input to echo back').setMaxLength(25)),
+  async execute(interaction) {
+    await interaction.reply(interaction.options.getString('input'));
+  },
+};
